deea924200ba436161cd7a9e7c4281de6e0ff760
Aldric <221369876+AldricPdn@users.noreply.github.com>
2025-07-17 17:37:27 +0200

    Update index.ts

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import settings from './settings'
+import product from './product' // <-- ajoute cette ligne
 
-export const schemaTypes = [post, author, category, settings, blockContent]
+export const schemaTypes = [post, author, category, settings, blockContent, product] // <-- ajoute product ici
